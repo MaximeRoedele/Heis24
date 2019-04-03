03af3cfe1c4c535f9ef2c8934ce31bd6ab030990
MaximeRoedele <47213803+MaximeRoedele@users.noreply.github.com>
2019-04-03 14:46:31 +0200

    Update control.h

--- source/control.h
+++ source/control.h
@@ -3,16 +3,12 @@
 #include "elev.h"
 #include "orders.h"
 
-int current_floor;
-int next_floor;
-elev_motor_direction_t motor_direction;
-
-//Kjører ned til første etasje den treffer. Oppdaterer current_floor.
-void init_movement(int *curret_floor);	
-//Bestemmer retning av heis for neste Order og utfører neste Order.
-void exec_order(elev_motor_direction_t motor_direction, int current_floor, int next_floor, bool order_at_floor);
+//KjÃ¸rer ned til fÃ¸rste etasje den treffer. Oppdaterer current_floor.
+void init_movement();	
+//Bestemmer retning av heis for neste Order og utfÃ¸rer neste Order.
+void exec_order();
 //Sjekker om heisen skal stoppe i passerede etasje.
-bool check_if_order(elev_motor_direction_t motor_direction, bool order_at_floor);
+bool check_if_order();
 //Stopper heisen
 void stop_at_floor();
 
